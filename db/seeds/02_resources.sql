272d037f40639fcbe4decc95f94263b061675eba
Alyssa Beaton <ehseebe@gmail.com>
2020-07-24 21:48:50 +0000

    Seeds for resources and categories

---
+++ db/seeds/02_resources.sql
@@ -0,0 +1,6 @@
+INSERT INTO resources (created_by, link, description, category_id, title, screenshot)
+VALUES (1, 'https://css-tricks.com/snippets/css/a-guide-to-flexbox/','Some cool CSS tricks', 6, 'A Complete Guide to Flexbox', 'www.google.com'),
+VALUES (2, 'https://www.akc.org/dog-breeds/boston-terrier/','The most amazing dogs!', 11, 'Boston Terrier', 'www.google.com'),
+VALUES (3, 'https://perchance.org/floridamanheadlinegenerator','FLORIDA MAN FOREVER', 11, 'Florida Man Headline Generator', 'www.google.com'),
+VALUES (4, 'https://www.youtube.com/watch?v=GFf7b8K1W0Y','Johann Sebastian Bach, Prelude and Fugue in A minor', 9, 'Jean Rondeau plays Bach', 'www.google.com'),
+VALUES (5, 'https://www.agar.io/#ffa','Avoid the blobs!!!', 11, 'Agar.io', 'www.google.com');
