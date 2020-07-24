300450cc79693ab0b5bd6051faf80e55403613d4
Alyssa Beaton <ehseebe@gmail.com>
2020-07-24 21:21:44 +0000

    Schemas

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,12 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
+
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name TEXT NOT NULL,
+  password TEXT,
+  bio TEXT,
+  avatar_url TEXT 
 );
---
+++ db/schema/07_comment_reviews.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS comment_reviews CASCADE;
+
+
+CREATE TABLE comment_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+  comment_id INTEGER REFERENCES comments(id) ON DELETE CASCADE,
+  like BOOLEAN DEFAULT FALSE,
+);
