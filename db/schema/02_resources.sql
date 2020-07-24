96c062182ec23393d6270f79891121f05cd49787
Alyssa Beaton <ehseebe@gmail.com>
2020-07-24 21:55:28 +0000

    Changed created_by

--- db/schema/02_resources.sql
+++ db/schema/02_resources.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS resources;
 CREATE TABLE resources
 (
   id SERIAL PRIMARY KEY NOT NULL,
-  created_by users.id,
+  created_by INTEGER REFERENCES users(id) ON DELETE CASCADE,
   link TEXT,
   description TEXT,
   category_id TINYINT,
