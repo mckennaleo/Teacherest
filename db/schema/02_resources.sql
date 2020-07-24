c0aae5925b9b5328d7946f627a82196d0fc9c416
Alyssa Beaton <ehseebe@gmail.com>
2020-07-24 22:03:12 +0000

    Changed category from tinyint to categories(id)

--- db/schema/02_resources.sql
+++ db/schema/02_resources.sql
@@ -5,7 +5,7 @@ CREATE TABLE resources
   created_by INTEGER REFERENCES users(id) ON DELETE CASCADE,
   link TEXT,
   description TEXT,
-  category_id TINYINT,
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
   title TEXT,
   screenshot TEXT,
   created_at TIMESTAMP DEFAULT now()
