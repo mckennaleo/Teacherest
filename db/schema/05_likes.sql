2910bcdfb394ab4ea17e52cd1bd257ddbbf3e245
mckennaleo <mckenna.leo@gmail.com>
2020-07-25 11:27:24 -0400

    added a likes seed

--- db/schema/05_likes.sql
+++ db/schema/05_likes.sql
@@ -3,7 +3,7 @@ CREATE TABLE likes
 (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  ressource_id INTEGER REFERENCES ressources(id) ON DELETE CASCADE,
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
   liked BOOLEAN,
   created_at TIMESTAMP DEFAULT now()
 );
---
+++ db/seeds/05-table.sql
@@ -0,0 +1,10 @@
+INSERT INTO likes
+  (user_id, resource_id, liked)
+VALUES
+  (1, 3, TRUE),
+  (4, 2, TRUE),
+  (4, 1, TRUE),
+  (3, 5, TRUE),
+  (5, 1, TRUE),
+  (2, 4, TRUE),
+  (5, 1, TRUE)
