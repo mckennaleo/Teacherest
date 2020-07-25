3ab300c27850a9c6c5ce2d68dd5580ae13660894
mckennaleo <mckenna.leo@gmail.com>
2020-07-25 11:23:14 -0400

    added a comment_reviews seed

---
+++ db/seeds/07_comment_reviews.sql
@@ -0,0 +1,11 @@
+INSERT INTO comment_reviews
+  (user_id, comment_id, liked)
+VALUES
+  (1, 5, TRUE),
+  (4, 10, TRUE),
+  (5, 4, TRUE),
+  (3, 5, TRUE),
+  (5, 5, TRUE),
+  (1, 1, TRUE),
+  (6, 3, TRUE),
+  (6, 6, TRUE)
