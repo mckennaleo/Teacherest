40a9387492d7c7872d94dabce70fc3a10b5a6115
mckennaleo <mckenna.leo@gmail.com>
2020-07-25 11:11:55 -0400

    Added more seeds

---
+++ db/seeds/04_comments.sql
@@ -0,0 +1,13 @@
+INSERT INTO comments
+  (user_id, resource_id, comment)
+VALUES
+  (1, 1, 'I love it!'),
+  (3, 1, 'Not bad at all'),
+  (4, 2, 'I think it''s superfluous'),
+  (5, 2, 'Great resource.'),
+  (2, 3, 'Good stuff.'),
+  (4, 3, 'I like it.'),
+  (5, 4, 'fantastic!'),
+  (2, 4, 'WoW!!!'),
+  (3, 5, 'This is the most incredible thing I''ve seen!'),
+  (4, 5, 'Very impressive.')
