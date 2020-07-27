3b4fca2a79b29bc56125d2392bc59538708d8f36
Francis Pellerin-Venne <francispellerinvenne@hotmail.fr>
2020-07-27 12:36:03 -0400

    added a toggle button

---
+++ routes/categories.js
@@ -0,0 +1,46 @@
+/*
+ * All routes for Categories are defined here
+ * Since this file is loaded in server.js into api/categories,
+ *   these routes are mounted onto /categories
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router = express.Router();
+
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+
+    db.query(`SELECT * FROM categories;`)
+      .then(data => {
+        const categories = data.rows;
+        res.json({ categories });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.get("/search", (req, res) => {
+
+    const searchQuery = req.query.s;
+
+    db.query(`SELECT * FROM categories WHERE name ILIKE $1;`, [searchQuery])
+      .then(data => {
+        const categories = data.rows;
+        res.json({ categories });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  return router;
+};
+
+
