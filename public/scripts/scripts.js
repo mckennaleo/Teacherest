1146f2984d338b5eda7cc3ef777a5a7ceb1a9323
Alyssa Beaton <ehseebe@gmail.com>
2020-07-27 18:58:35 +0000

    Implemented rough version of createResourceElement

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,8 +1,50 @@
 const renderResource = (data) => {
   for (let item of data) {
-    $('.display').append(`<p>${item.title}</p>`); 
+    $('.display').append(createResourceElement(item)); 
   }
 };
+
+const createResourceElement = (item) => {
+  //tweet posting date from database
+  let dbDate = item.created_at;
+
+  //calculation of relative time using moment.js
+  let readableDate = moment(dbDate).fromNow();
+
+  //html for posted tweets
+  const $resource = `
+  <article class='article-tweet'>
+      <header class='article-tweet-header'>
+        <span class='tweet-profile'>
+          <span class='tweet-profile-pic'>
+            <img src="${item.screenshot}" alt="profile picture" width="35" height="35">
+              </span>
+              <span class='tweet-profile-name'>
+                  ${item.title}
+            </span>
+        </span> 
+              <span class='tweet-handle'>
+              ${item.title}
+            </span>
+      </header>
+        <div class="tweet-content">
+          <p>${item.description}</p>
+        </div>
+        <footer class='article-tweet-footer'>
+          <span class='tweet-date'>
+            ${readableDate}
+            </span>
+          <span class='tweet-icons'>
+            <img src="/images/flag-icon.png" alt="flag-icon" width="10" height="10">
+            <img src="/images/retweet-icon.png" alt="retweet-icon" width="10" height="10">
+            <img src="/images/heart-icon.png" alt="heart-icon" width="10" height="10">
+          </span>
+        </footer>
+  </article>`;
+
+  return $resource;
+};
+
 
 $(document).ready(function () {
   $('.search-form').submit(function(e) {
@@ -21,7 +63,7 @@ $(document).ready(function () {
   const loadResources = () => {
     $.getJSON('/api/widgets/all', (response) => {
       console.log('success');
-      console.log('what is this', response)
+      //response.data because thats what the getter is returning
       renderResource(response.data);
     })
       .done(function() {
