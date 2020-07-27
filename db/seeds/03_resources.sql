609dfb2abde4f6d5d66e269a8cbb2cb99081bdb0
Alyssa Beaton <ehseebe@gmail.com>
2020-07-27 19:23:48 +0000

    Updated seeds with images for css styling

--- db/seeds/03_resources.sql
+++ db/seeds/03_resources.sql
@@ -1,8 +1,8 @@
 INSERT INTO resources
   (created_by, link, description, category_id, title, screenshot)
 VALUES
-  (1, 'https://css-tricks.com/snippets/css/a-guide-to-flexbox/', 'Some cool CSS tricks', 6, 'A Complete Guide to Flexbox', 'www.google.com'),
-  (2, 'https://www.akc.org/dog-breeds/boston-terrier/', 'The most amazing dogs!', 11, 'Boston Terrier', 'www.google.com'),
-  (3, 'https://perchance.org/floridamanheadlinegenerator', 'FLORIDA MAN FOREVER', 11, 'Florida Man Headline Generator', 'www.google.com'),
-  (4, 'https://www.youtube.com/watch?v=GFf7b8K1W0Y', 'Johann Sebastian Bach, Prelude and Fugue in A minor', 9, 'Jean Rondeau plays Bach', 'www.google.com'),
-  (5, 'https://www.agar.io/#ffa', 'Avoid the blobs!!!', 11, 'Agar.io', 'www.google.com');
+  (1, 'https://css-tricks.com/snippets/css/a-guide-to-flexbox/', 'Some cool CSS tricks', 6, 'A Complete Guide to Flexbox', 'https://media.wired.com/photos/5be4cd03db23f3775e466767/125:94/w_2375,h_1786,c_limit/books-521812297.jpg'),
+  (2, 'https://www.akc.org/dog-breeds/boston-terrier/', 'The most amazing dogs!', 11, 'Boston Terrier', 'https://static01.nyt.com/images/2019/12/17/books/review/17fatbooks/17fatbooks-mobileMasterAt3x.jpg'),
+  (3, 'https://perchance.org/floridamanheadlinegenerator', 'FLORIDA MAN FOREVER', 11, 'Florida Man Headline Generator', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSbd9dFK2_bw1NyhBKrjKHvPXGKhlauXsndwg&usqp=CAU'),
+  (4, 'https://www.youtube.com/watch?v=GFf7b8K1W0Y', 'Johann Sebastian Bach, Prelude and Fugue in A minor', 9, 'Jean Rondeau plays Bach', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTwQWJaEAGBMocmYs50IcYrT0AEjaWqmOgflQ&usqp=CAU'),
+  (5, 'https://www.agar.io/#ffa', 'Avoid the blobs!!!', 11, 'Agar.io', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTpotWxtMG9mld21UAR4v5z-8A8vZodVzEZsw&usqp=CAU');
