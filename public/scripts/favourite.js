24edf19bcd59908f7c5bf4e6da07035cd608760b
Alyssa Beaton <ehseebe@gmail.com>
2020-07-29 20:28:48 +0000

    Writing script for favourite

---
+++ public/scripts/favourite.js
@@ -0,0 +1,14 @@
+//AJAX post request to resource/id?
+//if no like, post to add, if like post to remove
+
+
+$(document).ready(function() {
+
+  const $favouriteBtn = ('.favourite-button');
+
+  $($favouriteBtn).on('click', function(event) {
+    event.preventDefault();
+    
+  })
+
+});
