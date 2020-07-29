47b9b426998037202da31ac0750506b145883788
Alyssa Beaton <ehseebe@gmail.com>
2020-07-29 00:02:48 +0000

    Copied from this file for new route

--- routes/widgets.js
+++ routes/widgets.js
@@ -41,6 +41,22 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  router.get("/comments", (req, res) => {
+    // console.log("WHAT IS THIS:", req.body)
+    //let query = `SELECT * FROM resources`;
+    getAllResources(req)
+      .then(data => {
+        //data = query response from getAllResources!
+        // console.log(data)
+        res.json({ data });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
   
   return router;
 };
